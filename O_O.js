a8cd1ed3dad07ad4b1bdd12c2d65f8dcf0a84ffa
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-08-04 00:23:17 +0530

    Added new files

---
+++ O_O.js
@@ -0,0 +1,2 @@
+//hume sikhna hai factories bana mtlb ki ek baar blueprint ban do ki hrr boject kesa dikhega orr hum log new new object with diffrent  values bana payenge 
+//yahi upr upr say  pura kaaam hai oops mai
