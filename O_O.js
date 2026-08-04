7a98c19c8c0785bf14c291e779bcbeee4fb6aeea
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-08-04 19:40:54 +0530

    Added new files

--- O_O.js
+++ O_O.js
@@ -1,2 +1,11 @@
 //hume sikhna hai factories bana mtlb ki ek baar blueprint ban do ki hrr boject kesa dikhega orr hum log new new object with diffrent  values bana payenge 
 //yahi upr upr say  pura kaaam hai oops mai
+
+function kutta( breed , color , age , price){
+    this.breed= breed;
+    this.color=color;
+    this.age= age;
+    this.peice= price;
+}
+let dog1= new kutta("labra", "black", 1.5,2000);
+let dog2= new kutta("INDI", "black/black", 3,3000);
