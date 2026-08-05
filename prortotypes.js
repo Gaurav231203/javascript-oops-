3dd1373c93ae43cee4a1086fd684d32633d7e894
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-08-06 00:57:28 +0530

    Added new files

--- prortotypes.js
+++ prortotypes.js
@@ -1,20 +1,28 @@
 /*prototype kya hai
 
-Har function (jab constructor ki tarah use ho) ke paas ek (prototype) property hoti hai — ye ek object hai jahan hum shared methods rakhte hain, taake har naya instance apna alag copy na bana kar sabse share kare.*/
+prototyoes ek object hai jo hr javascript object kay kaath attached hota hai it tells ki object ko kon si property/mathod automaticlly mielngi bine define kiye */
+
+//======>PROTOTYPE is like a shared memeory
+
 
 function Kutta( breed , color , age , price){
     this.breed= breed;
     this.color=color;
     this.age= age;
     this.peice= price;
-    this.write = function (text){ // ek mathod banya 
+  
+};
+Kutta.prototype.write = function (text){ // ek mathod banya 
         //now create h1
+        
         let h1 = document.createElement("h1");
+        console.log(h1)
         h1.textContent=text;
-        h1.style.color=color;
+        h1.style.color=this.color;
         document.body.append(h1)
+        
+
 
     };
-}
 let dog1= new Kutta("labra", "black", 1.5,2000);
 let dog2= new Kutta("INDI", "brown", 3,3000);
